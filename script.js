b143be2aca19a07c8d10f2543a49b42b1d9f5f08
Nutsubish <147431395+Nutsubish@users.noreply.github.com>
2024-03-29 18:26:55 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+
+
+function  me(){
+   link = "https://www.facebook.com/nikoloz.nutsubidze.90"
+   window.open(link, '_blank')
+}
+
+
+let Home = document.getElementById("Home");
+let About = document.getElementById("About");
+let Images = document.getElementById("Images");
+let btn = document.getElementById("Btn")
+
+
+function goto(sectionId) {
+  const section = document.getElementById(sectionId);
+  section.scrollIntoView({ behavior: 'smooth' });
+}
+
+About.addEventListener("click", function() {
+  goto("AboutSec"); 
+});
+
+Images.addEventListener("click", function() {
+  goto("Imagessec"); 
+});
+
+btn.addEventListener("click",function(){
+   goto("navbar")
+})
+
+Home.addEventListener("click",function(){
+   goto("navbar")
+})
